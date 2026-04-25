c74f77888cd0577af43fb20f55346840c35793b8
R4Y4N3 <108823783+R4Y4N3@users.noreply.github.com>
2026-04-25 09:12:37 +0100

    Update registration URL in event.js

--- app/lib/event.js
+++ app/lib/event.js
@@ -1,4 +1,4 @@
-export const REGISTRATION_URL = "https://form.typeform.com/to/JDssZE8B";
+export const REGISTRATION_URL = "https://docs.google.com/forms/d/1WxuGqHY0VGVSp5lHpDIcdFV-UhHiPQJBAA9fEpC1LlE";
 export const EVENT_START_ISO = "2026-04-30T09:00:00+01:00";
 export const EVENT_DATE_LABEL = "April 30 - May 2, 2026";
 export const EVENT_LOCATION_LABEL = "ESTIN, Amizour, Bejaia";
